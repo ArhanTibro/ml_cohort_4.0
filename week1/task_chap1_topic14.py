9249870ef296a19ecec9692d8813bcaf0bf9925b
Arhan Tibro <arhantibro2004@gmail.com>
2025-03-09 15:28:24 +0600

    task  done for the topic on function in python

---
+++ week1/task_chap1_topic14.py
@@ -0,0 +1,22 @@
+def nid(name,faher_name,mother_name,age,gender,present_address,permanent_address):
+    print('Name : '+name)
+    print('Father Name : '+faher_name)
+    print('Mother Name : '+mother_name)
+    print('Age : '+age)
+    print('Gender : '+gender)
+    print('Present Address : '+present_address)
+    print('Permanent Address : '+permanent_address)
+
+
+print('provide your informations : ')
+nm=input('name : ')
+fn=input('father name : ')
+mn=input('mother name : ')
+ag=input('age : ')
+gen=input('gender(Male or Female) : ')
+present=input('present address : ')
+permanent=input('permanent address : ')
+
+
+print('\n\nnid details : \n')
+nid(nm,fn,mn,ag,gen,present,permanent)
